913d597896aba9a8f044f7903a9794545dcb9924
SPAMIK356 <maximbondarenko20@gmail.com>
2025-09-30 00:34:58 +0300

    Bug fixed

--- backend/ai_aggregator/urls.py
+++ backend/ai_aggregator/urls.py
@@ -1,9 +1,10 @@
 import os
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import JsonResponse
+from django.views.static import serve as media_serve
 
 
 def health(_request):
@@ -19,5 +20,10 @@ urlpatterns = [
 # Serve media in dev and optionally in containers when SERVE_MEDIA=1
 if getattr(settings, "DEBUG", False) or os.getenv("SERVE_MEDIA", "1") == "1":
 	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Always provide a fallback explicit media route (useful in containers)
+urlpatterns += [
+	re_path(r"^media/(?P<path>.*)$", media_serve, {"document_root": settings.MEDIA_ROOT}),
+]
 
 
