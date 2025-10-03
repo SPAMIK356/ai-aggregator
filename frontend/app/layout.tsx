0f6471ecb3ee0eaeb274b344549115f35082bcdc
SPAMIK356 <maximbondarenko20@gmail.com>
2025-10-03 19:03:33 +0300

    Fixed font

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,12 +4,6 @@ import Image from 'next/image';
 import newsIcon from '../icons/News_site.png';
 import authorsIcon from '../icons/Autors.png';
 import contactIcon from '../icons/Contact.png';
-import localFont from 'next/font/local';
-
-const digital = localFont({
-	src: '../fonts/DIGITALPIXELV4-REGULAR.OTF',
-	display: 'swap',
-});
 
 export const metadata = {
 	title: '2049.news',
@@ -19,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="ru">
-			<body className={digital.className}>
+			<body>
 				<header className="site-header">
 					<div className="container">
 					<nav className="nav">
